09c57bcbab0e25fc32ecfe2e1b35059bff1f284d
Oğuzhan KÖMCÜ <96787308+oguzhanKomcu@users.noreply.github.com>
2022-01-16 01:42:41 +0300

    Update Form1.cs

---
+++ 04_FoR_CYCLE/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace _04_FoR_CYCLE
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFactorial = new System.Windows.Forms.Button();
+            this.textSayi1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btExample3 = new System.Windows.Forms.Button();
+            this.btnPrimeNumbers = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnFactorial);
+            this.groupBox1.Controls.Add(this.textSayi1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(26, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(258, 170);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Factorial Calculation";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number :";
+            // 
+            // btnFactorial
+            // 
+            this.btnFactorial.Location = new System.Drawing.Point(102, 103);
+            this.btnFactorial.Name = "btnFactorial";
+            this.btnFactorial.Size = new System.Drawing.Size(141, 36);
+            this.btnFactorial.TabIndex = 1;
+            this.btnFactorial.Text = "CALCULATİON";
+            this.btnFactorial.UseVisualStyleBackColor = true;
+            this.btnFactorial.Click += new System.EventHandler(this.btnFactorial_Click);
+            // 
+            // textSayi1
+            // 
+            this.textSayi1.Location = new System.Drawing.Point(102, 47);
+            this.textSayi1.Name = "textSayi1";
+            this.textSayi1.Size = new System.Drawing.Size(141, 33);
+            this.textSayi1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(453, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(167, 319);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btExample3
+            // 
+            this.btExample3.Location = new System.Drawing.Point(319, 26);
+            this.btExample3.Name = "btExample3";
+            this.btExample3.Size = new System.Drawing.Size(107, 41);
+            this.btExample3.TabIndex = 4;
+            this.btExample3.Text = "NBA W-C";
+            this.btExample3.UseVisualStyleBackColor = true;
+            this.btExample3.Click += new System.EventHandler(this.btExample3_Click);
+            // 
+            // btnPrimeNumbers
+            // 
+            this.btnPrimeNumbers.Location = new System.Drawing.Point(319, 86);
+            this.btnPrimeNumbers.Name = "btnPrimeNumbers";
+            this.btnPrimeNumbers.Size = new System.Drawing.Size(108, 37);
+            this.btnPrimeNumbers.TabIndex = 5;
+            this.btnPrimeNumbers.Text = "Prime Numbers";
+            this.btnPrimeNumbers.UseVisualStyleBackColor = true;
+            this.btnPrimeNumbers.Click += new System.EventHandler(this.btnPrimeNumbers_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 450);
+            this.Controls.Add(this.btnPrimeNumbers);
+            this.Controls.Add(this.btExample3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button btnFactorial;
+        private TextBox textSayi1;
+        private Label label1;
+        private ListBox listBox1;
+        private Button btExample3;
+        private Button btnPrimeNumbers;
+    }
+}
---
+++ 04_FoR_CYCLE/Form1.cs
@@ -0,0 +1,72 @@
+namespace _04_FoR_CYCLE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFactorial_Click(object sender, EventArgs e)
+        {
+
+            //Let's find the factorial of the entered number.
+
+            int carpim = 1;
+            int sayi1 = Convert.ToInt32(textSayi1.Text);
+
+            for (int i = 1; i <= sayi1; i++)
+            {
+                carpim = i * carpim;
+                if (i == sayi1)
+                    MessageBox.Show($"Conclusion :{sayi1}! = {carpim}");
+
+            }
+
+
+        }
+
+
+        private void btExample3_Click(object sender, EventArgs e)
+        {
+            //Let's print the teams in the nba western conference.
+            string[] nba_teams = {"Denver Nuggets", "Minnesota Timberwolves", "Oklahoma City Thunder", "Portland Trail Blazers", 
+                                         "Utah Jazz", "Dallas Mavericks" , "Houston Rockets","Memphis Grizzlies","New Orleans Hornets","San Antonio Spurs",
+                                     "Golden State Warriors","Los Angeles Clippers","Los Angeles Lakers","Phoneix Suns","Sacramento Kings"};
+
+            for (int i = 0; i < nba_teams.Length; i++)
+            {
+                listBox1.Items.Add(nba_teams[i]);
+            }
+
+        }
+
+        private void btnPrimeNumbers_Click(object sender, EventArgs e)
+        {
+            //Let's print the actual numbers up to 100 on the screen.
+
+            int sayi = 100;
+            int sayac;
+
+            for (int i = 10; i < sayi; i++)
+            {
+                sayac = 0;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sayac++;
+                    }
+                }
+                if (sayac == 0)
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+
+
+
+
+        }
+    }
+}
